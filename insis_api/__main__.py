7fa616452f159c10d3e36c04ce1c42176d9a2d42
Matěj Čermák <m.cermak17@gmail.com>
2018-05-20 18:59:45 +0200

    https with self signed ssl cert

--- insis_api/__main__.py
+++ insis_api/__main__.py
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Insis api'})
-    app.run(port=8080, debug=True)
+    app.run(port=8080, ssl_context='adhoc', debug=True)
 
 
 if __name__ == '__main__':
