e5b31fe6c86c68ad8f64715ea762d134d3af004b
Matěj Čermák <m.cermak17@gmail.com>
2018-05-24 16:50:16 +0200

    optimized imports

--- insis_api/test/test_exam_controller.py
+++ insis_api/test/test_exam_controller.py
@@ -13,6 +13,48 @@ from insis_api.test import BaseTestCase
 class TestExamController(BaseTestCase):
     """ExamController integration test stubs"""
 
+    def test_disenroll_exam_by_id(self):
+        """Test case for disenroll_exam_by_id
+
+        Disenroll exam with id
+        """
+        body = User()
+        response = self.client.open(
+            '/exam/{examId}/disenroll'.format(examId=789),
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_enroll_exam_by_id(self):
+        """Test case for enroll_exam_by_id
+
+        Enroll exam with id
+        """
+        body = User()
+        response = self.client.open(
+            '/exam/{examId}/enroll'.format(examId=789),
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_enrolled_exam(self):
+        """Test case for get_enrolled_exam
+
+        Get enrolled exams
+        """
+        body = User()
+        response = self.client.open(
+            '/exam/enrolled',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
     def test_get_exam(self):
         """Test case for get_exam
 
@@ -21,6 +63,20 @@ class TestExamController(BaseTestCase):
         body = User(os.environ['USERNAME'], os.environ['PASS'])
         response = self.client.open(
             '/exam',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_exam_by_id(self):
+        """Test case for get_exam_by_id
+
+        Get exam by id
+        """
+        body = User()
+        response = self.client.open(
+            '/exam/{examId}'.format(examId=789),
             method='POST',
             data=json.dumps(body),
             content_type='application/json')
